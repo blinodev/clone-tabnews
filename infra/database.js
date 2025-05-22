42bdedfa8c31d95b671f2c4c2b0d20b9b5b07be8
linojan <linojanes@gmail.com>
2025-05-22 20:32:14 -0300

    Resolvendo conflitos após pull

--- infra/database.js
+++ infra/database.js
@@ -6,10 +6,14 @@ async function query(queryObject) {
     port: 5432,
     user: "postgres",
     database: "postgres",
-    password: "local_password",
+    password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
   const result = await client.query(queryObject);
   await client.end();
   return result;
 }
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,14 +1,8 @@
 import database from "../../../../infra/database.js";
-
-async function status(request, response) {
-  try {
-    const result = await database.query("SELECT 1 + 1 as sum;");
-    console.log(result.rows);
-    response.status(200).json({ chave: "São acima da média" });
-  } catch (error) {
-    console.error("Erro no endpoint /status:", error);
-    response.status(500).json({ error: "Erro interno no servidor" });
-  }
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
+  res.status(200).json({ status: "ok" });
 }
 
 export default status;
