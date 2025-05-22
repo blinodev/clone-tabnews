c76c3a69eaa56830b99961e5f6d31decf88b0bb6
linojan <linojanes@gmail.com>
2025-05-22 15:53:42 -0300

    update

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,14 @@
-function status(request, response) {
-  response.status(200).json({ chave: "São acima da média" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  try {
+    const result = await database.query("SELECT 1 + 1 as sum;");
+    console.log(result.rows);
+    response.status(200).json({ chave: "São acima da média" });
+  } catch (error) {
+    console.error("Erro no endpoint /status:", error);
+    response.status(500).json({ error: "Erro interno no servidor" });
+  }
 }
 
 export default status;
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 970892cd1182a5a0c219922508eb1e783b26f2f5
