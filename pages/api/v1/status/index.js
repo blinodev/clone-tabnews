065e8d0c1f11875e2ed9dea55ba1d6529aad10d7
blinodev <linojanes@gmail.com>
2025-06-30 21:31:40 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,34 +1,43 @@
+// Controller (entrada) usa as ferramentas disponíveis
+
 import database from "infra/database.js";
 
+// Teste de query simples
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+  `
+  `;
+  // Versão do PostgreSQL
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseVersion = await database.query("SHOW server_version");
-  const databaseVersionValue = databaseVersion.rows[0].server_version;
-
-  const databaseMaxConnectionsResult = await database.query(
+  // Máximo de conexões permitidas
+  const databaseMaxConnectionResult = await database.query(
     "SHOW max_connections;",
   );
-  const databaseMaxConnectionsValue =
-    databaseMaxConnectionsResult.rows[0].max_connections;
-  //forma de evitar sqlInjection (node-postgres)
+  const databaseMaxConnectionValue =
+    databaseMaxConnectionResult.rows[0].max_connections;
 
+  // Conexões ativas no momento
   const databaseName = process.env.POSTGRES_DB;
+  // Enviando objeto abaixo para método query
   const databaseOpenedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
 
-  const databaseOpenedConnectionsValue =
+  const databaseOpenedConnectionValue =
     databaseOpenedConnectionsResult.rows[0].count;
+  // View manipula resposta e retorna
+  // Interface pública http://localhost:3000/api/v1/status
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
-        max_connections: parseInt(databaseMaxConnectionsValue),
-        open_connections: databaseOpenedConnectionsValue,
+        max_connections: parseInt(databaseMaxConnectionValue),
+        opened_connections: databaseOpenedConnectionValue,
       },
     },
   });
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -2,12 +2,18 @@ test("GET to /api/v1/status should be return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
-  const responseBody = await response.json("Conseguiu conectar");
+  const responseBody = await response.json();
+  //console.log(responseBody);
+  // Verifica se existe a chave 'updated_at' no JSON de resposta
+  //expect(responseBody.updated_at).toBeDefined();
+  // 'undefinned' indefinido
+  // typeof responseBody.updated_at !== 'undefined'
 
-  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
   expect(responseBody.dependencies.database.version).toEqual("16.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.open_connections).toEqual(1);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
+
